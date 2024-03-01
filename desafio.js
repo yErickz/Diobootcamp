c7fdebde623c78372fe719ffb420ec7c5e404dc6
yErickz <ericoliveira11338@gmail.com>
2024-02-29 22:51:14 -0300

    Desafio 01

---
+++ desafio.js
@@ -0,0 +1,30 @@
+    let NomeDoHeroi = "yErickz"
+    let XpDoPersonagem = 1
+
+    let nivelDoHeroi
+
+    if(XpDoPersonagem <= 1000){
+        nivelDoHeroi = "Ferro"
+    } else if(XpDoPersonagem <= 1001){
+        nivelDoHeroi = "Bronze"
+    }
+    else if(XpDoPersonagem <= 2001){
+        nivelDoHeroi = "Prata"
+    }
+    else if(XpDoPersonagem <= 5000){
+        nivelDoHeroi = "Ouro"
+    }
+    else if(XpDoPersonagem <= 7001){
+        nivelDoHeroi = "Platina"
+    }
+    else if(XpDoPersonagem <= 8000){
+        nivelDoHeroi = "Ascedente"
+    }
+    else if(XpDoPersonagem <= 9001){
+        nivelDoHeroi = "Imortal"
+    }
+    else if(XpDoPersonagem <= 10000){
+        nivelDoHeroi = "Radiante"
+    }
+    
+    console.log ("O Herói de nome " + NomeDoHeroi + "está no nível de " + nivelDoHeroi)
