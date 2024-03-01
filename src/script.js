2e51d5c74ab895867de2522b932c810779e07cb8
yErickz <ericoliveira11338@gmail.com>
2024-02-29 21:14:27 -0300

    commite hehe

---
+++ src/script.js
@@ -0,0 +1,11 @@
+console.log("Digita o nome do seu jogador") 
+
+let Nickname = "_yErickz"
+
+console.log("Seja Bem vindo!!" + Nickname)
+
+const notificacao = "Pokemon Go diz: "
+
+//saida
+console.log(notificacao + "tem um novo pokemon na região")
+console.log(notificacao + "você foi derrotado por um líder")
